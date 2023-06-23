8b1cf56fba3059f03a6bb8f183bc6f580ed20c67
dal-liu <daniel.albert.liu@gmail.com>
2023-06-22 22:58:20 -0400

    Completed 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,14 +3,38 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, item) => {
-    return sum + item.likes
+  const reducer = (accum, blog) => {
+    return accum + blog.likes
   }
 
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (currentFavorite, blog) => {
+    if (blog.likes > currentFavorite.likes) {
+      return {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    } else {
+      return currentFavorite
+    }
+  }
+
+  const initial = blogs.length === 0
+    ? null
+    : {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    }
+  return blogs.reduce(reducer, initial)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
