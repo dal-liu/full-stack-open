d7f001abe79b8d0c470fac1043b4521e12912a32
dal-liu <daniel.albert.liu@gmail.com>
2023-09-13 23:24:27 -0500

    Completed 5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const hideWhenVisible = { display: showDetails ? 'none' : '' }
+  const showWhenVisible = { display: showDetails ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(true)} style={hideWhenVisible}>view</button>
+        <button onClick={() => setShowDetails(false)} style={showWhenVisible}>hide</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes}
+        <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
