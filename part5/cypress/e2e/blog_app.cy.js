ea6b509b9838c63b0081c95f3ee4aa015ed5bbab
dal-liu <daniel.albert.liu@gmail.com>
2023-09-16 14:04:19 -0500

    Completed 5.18

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'name1',
+      username: 'user1',
+      password: 'password1'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,27 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('user1')
+      cy.get('#password').type('password1')
+      cy.get('#login-button').click()
+
+      cy.contains('name1 logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('user1')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'name1 logged in')
+    })
   })
 })
