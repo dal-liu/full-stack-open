2f2b38e6d34a38587ea18999d21c27ceabc0a45d
dal-liu <daniel.albert.liu@gmail.com>
2023-09-18 01:43:18 -0500

    Completed 6.4

--- part6/anecdotes/src/reducers/anecdoteReducer.js
+++ part6/anecdotes/src/reducers/anecdoteReducer.js
@@ -33,6 +33,8 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : votedAnecdote
       )
+    case 'NEW_ANECDOTE':
+      return state.concat(action.payload)
     default:
       return state
   }
@@ -42,6 +44,17 @@ export const voteAnecdoteOf = (id) => {
   return {
     type: 'VOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content,
+      votes: 0,
+      id: getId()
+    }
   }
 }
 
