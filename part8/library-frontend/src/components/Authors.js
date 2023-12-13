3aa073d60727b2cedf9f3b45e41e0ccb80f14002
dal-liu <daniel.albert.liu@gmail.com>
2023-12-13 02:10:26 -0500

    Completed 8.10-8.11

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,14 +1,48 @@
-import { gql, useQuery } from '@apollo/client'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+import { useState } from 'react'
 
-const ALL_AUTHORS = gql`
-  query {
-    allAuthors {
-      name
-      born
-      bookCount
-    }
+const AuthorForm = () => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
+  const submit = async event => {
+    event.preventDefault()
+
+    editAuthor({
+      variables: { name, setBornTo: parseInt(born) },
+    })
+
+    setName('')
+    setBorn('')
   }
-`
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
 
 const Authors = props => {
   const result = useQuery(ALL_AUTHORS)
@@ -42,6 +76,7 @@ const Authors = props => {
           ))}
         </tbody>
       </table>
+      <AuthorForm />
     </div>
   )
 }
--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, ADD_BOOK } from '../queries'
 
 const NewBook = props => {
   const [title, setTitle] = useState('')
@@ -6,6 +8,9 @@ const NewBook = props => {
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+  })
 
   if (!props.show) {
     return null
@@ -14,6 +19,9 @@ const NewBook = props => {
   const submit = async event => {
     event.preventDefault()
 
+    addBook({
+      variables: { title, author, published: parseInt(published), genres },
+    })
     console.log('add book...')
 
     setTitle('')
