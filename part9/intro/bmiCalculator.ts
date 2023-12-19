234a75fcd0015e9144d37c7e914635082a0a8961
dal-liu <daniel.albert.liu@gmail.com>
2023-12-18 19:39:09 -0500

    Completed 9.3

--- part9/intro/bmiCalculator.ts
+++ part9/intro/bmiCalculator.ts
@@ -1,3 +1,24 @@
+import { isNumber } from './utils';
+
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseBmiArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (isNumber(args[2]) && isNumber(args[3])) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
+
 const calculateBmi = (height: number, weight: number): string => {
   if (height === 0) throw new Error("Can't divide by 0!");
 
@@ -21,4 +42,13 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-console.log(calculateBmi(180, 74));
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/intro/exerciseCalculator.ts
+++ part9/intro/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+import { isNumber } from './utils';
+
 interface Result {
   periodLength: number;
   trainingDays: number;
@@ -7,6 +9,33 @@ interface Result {
   target: number;
   average: number;
 }
+
+interface ExerciseValues {
+  exerciseHours: number[];
+  target: number;
+}
+
+const parseExerciseArguments = (args: string[]): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const exerciseHours = args.slice(3).map((d) => {
+    if (isNumber(d)) {
+      return Number(d);
+    } else {
+      throw new Error(`Provided value ${d} was not a number`);
+    }
+  });
+
+  if (isNumber(args[2])) {
+    return {
+      exerciseHours,
+      target: Number(args[2]),
+    };
+  } else {
+    throw new Error('Provided target was not a number');
+  }
+};
+
 const calculateExercises = (
   exerciseHours: number[],
   target: number
@@ -36,4 +65,13 @@ const calculateExercises = (
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+  const { exerciseHours, target } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(exerciseHours, target));
+} catch (error: unknown) {
+  let errorMessage = 'An error occurred.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
